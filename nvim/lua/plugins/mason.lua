39b32d1b2781b457fe798213b97665b001256243
marcellmueller <marcel@button.is>
2024-08-28 08:06:43 -0700

    chore: update nvim config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,24 +1,26 @@
+-- customize mason plugins
 return {
+  -- use mason-lspconfig to configure LSP installations
   {
-    "williamboman/mason.nvim",
-    cmd = {
-      "Mason",
-      "MasonInstall",
-      "MasonUninstall",
-      "MasonUninstallAll",
-      "MasonLog",
-      "MasonUpdate", -- AstroNvim extension here as well
-      "MasonUpdateAll", -- AstroNvim specific
-    },
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_uninstalled = "✗",
-          package_pending = "⟳",
-        },
-      },
+      -- ensure_installed = { "lua_ls" },
     },
-    config = require "plugins.configs.mason",
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = {
+      -- ensure_installed = { "prettier", "stylua" },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = {
+      -- ensure_installed = { "python" },
+    },
   },
 }
